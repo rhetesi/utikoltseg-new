b5ad662a7848a5fd1f2f19e1da63f1bbf60eb2f5
Hetesi Roland <rhetesi@gmail.com>
2022-08-22 14:29:21 +0200

    Basic HTML, CSS, JS

--- scripts.js
+++ scripts.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const today = new Date();
+let actualMonth = today.getMonth();
+let beforeMonth = actualMonth - 1;
+let afterMonth = actualMonth + 1;
+
+const basedatas = {
+    name: '',
+    city: '',
+    address: '',
+    distance: '',
+    vehicle: '',
+    plate: ''
+};
+
