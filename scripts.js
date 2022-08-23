fd24d0424bcca2715406b6d27ae015ca518f7b5f
Hetesi Roland <rhetesi@gmail.com>
2022-08-23 12:35:37 +0200

    month slection with ternary operator

--- scripts.js
+++ scripts.js
@@ -1,11 +1,14 @@
 'use strict';
 
 const today = new Date();
-let actualMonth = today.getMonth();
-let beforeMonth = actualMonth - 1;
-let afterMonth = actualMonth + 1;
+const actualMonth = today.getMonth();
+const beforeMonth = actualMonth - 1;
+const afterMonth = actualMonth + 1;
 const dateOptionLongHU = {year: 'numeric', month: 'long', day: 'numeric'};
-const dateOptionMonthHU = {month: 'long'};
+const dateOptionMonthHU = { month: 'long' };
+const beforeMonthShortForm = new Date(today.getFullYear(), beforeMonth, today.getDay()).toLocaleDateString('hu-HU', dateOptionMonthHU);
+const actualMonthShortForm = new Date(today.getFullYear(), actualMonth, today.getDay()).toLocaleDateString('hu-HU', dateOptionMonthHU);
+const afterMonthShortForm = new Date(today.getFullYear(), afterMonth, today.getDay()).toLocaleDateString('hu-HU', dateOptionMonthHU);
 
 const basedatas = {
     name: '',
@@ -21,6 +24,8 @@ const monthSelectionButton = document.querySelector('.monthselection');
 const daysPicker = document.querySelector('.dayspicker');
 const printButton = document.querySelector('.print');
 
-monthSelect.insertAdjacentHTML('beforeend', new Date(today.getFullYear(), beforeMonth, today.getDay()).toLocaleDateString('hu-HU', dateOptionMonthHU));
-monthSelect.insertAdjacentHTML('beforeend', new Date(today.getFullYear(), actualMonth, today.getDay()).toLocaleDateString('hu-HU', dateOptionMonthHU));
-monthSelect.insertAdjacentHTML('beforeend', new Date(today.getFullYear(), afterMonth, today.getDay()).toLocaleDateString('hu-HU', dateOptionMonthHU));
+// monthSelect.insertAdjacentHTML('beforeend', new Date(today.getFullYear(), beforeMonth, today.getDay()).toLocaleDateString('hu-HU', dateOptionMonthHU));
+// monthSelect.insertAdjacentHTML('beforeend', new Date(today.getFullYear(), actualMonth, today.getDay()).toLocaleDateString('hu-HU', dateOptionMonthHU));
+// monthSelect.insertAdjacentHTML('beforeend', new Date(today.getFullYear(), afterMonth, today.getDay()).toLocaleDateString('hu-HU', dateOptionMonthHU));
+
+today.getDate() >= 20 ? monthSelect.insertAdjacentHTML('beforeend', `${actualMonthShortForm}`) : monthSelect.insertAdjacentHTML('beforeend', `${beforeMonthShortForm} , ${actualMonthShortForm}`) ;
