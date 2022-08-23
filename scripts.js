8c22d6d63ba298805ed388507e4242baebdfe303
Hetesi Roland <rhetesi@gmail.com>
2022-08-23 13:24:32 +0200

    how to select which months do we need

--- scripts.js
+++ scripts.js
@@ -28,4 +28,18 @@ const printButton = document.querySelector('.print');
 // monthSelect.insertAdjacentHTML('beforeend', new Date(today.getFullYear(), actualMonth, today.getDay()).toLocaleDateString('hu-HU', dateOptionMonthHU));
 // monthSelect.insertAdjacentHTML('beforeend', new Date(today.getFullYear(), afterMonth, today.getDay()).toLocaleDateString('hu-HU', dateOptionMonthHU));
 
-today.getDate() >= 20 ? monthSelect.insertAdjacentHTML('beforeend', `${actualMonthShortForm}`) : monthSelect.insertAdjacentHTML('beforeend', `${beforeMonthShortForm} , ${actualMonthShortForm}`) ;
+today.getDate() >= 20 ?
+    monthSelect.insertAdjacentHTML('beforeend', `${actualMonthShortForm}`) :
+    monthSelect.insertAdjacentHTML('beforeend', `${beforeMonthShortForm} , ${actualMonthShortForm}`);
+
+/**
+ * Ha nap kisebb/egyenlő 10, akkor előző hónap legyen kiválasztva és felajánlja az aktuális hónapot,
+ * ha 11 és 20 között, akkor aktuális hónap kiválasztva és felajánlja az előző hónapot,
+ * ha 21 vagy nagyobb, akkor csak az aktuális hónap jelenjen meg!!!
+ * 
+ * month <= 10 ?
+ *  before/radio(selected), actual/radio :
+ *  month >= 11 && month <= 20 ?
+ *      before/radio, actual/radio(selected) :
+ *      actual/radio(selected)
+ */
