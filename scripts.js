8716c5d949249161fa7265e26331c892d1fd1e8a
Hetesi Roland <rhetesi@gmail.com>
2022-08-23 12:00:19 +0200

    insert & format months

--- scripts.js
+++ scripts.js
@@ -4,6 +4,8 @@ const today = new Date();
 let actualMonth = today.getMonth();
 let beforeMonth = actualMonth - 1;
 let afterMonth = actualMonth + 1;
+const dateOptionLongHU = {year: 'numeriv', month: 'long', day: 'numeric'};
+const dateOptionMonthHU = {month: 'long'};
 
 const basedatas = {
     name: '',
@@ -18,3 +20,7 @@ const monthSelect = document.querySelector('.monthselect');
 const monthSelectionButton = document.querySelector('.monthselection');
 const daysPicker = document.querySelector('.dayspicker');
 const printButton = document.querySelector('.print');
+
+monthSelect.insertAdjacentHTML('beforeend', new Date(today.getFullYear(), beforeMonth, today.getDay()).toLocaleDateString('hu-HU', dateOptionMonthHU));
+monthSelect.insertAdjacentHTML('beforeend', new Date(today.getFullYear(), actualMonth, today.getDay()).toLocaleDateString('hu-HU', dateOptionMonthHU));
+monthSelect.insertAdjacentHTML('beforeend', new Date(today.getFullYear(), afterMonth, today.getDay()).toLocaleDateString('hu-HU', dateOptionMonthHU));
