0266fd1815762bb1add402ab43d70476cd9dd59b
Hetesi Roland <rhetesi@gmail.com>
2022-09-12 10:28:10 +0200

    del feat: lotto

--- assets/main.js
+++ assets/main.js
@@ -35,29 +35,3 @@ const monthSelect = document.querySelector('.monthselect');
 const daysPicker = document.querySelector('.dayspicker');
 const printButton = document.querySelector('.print');
 
-
-// Lottóhúzás
-const lottoArr = [];
-const lottoNums = [];
-for (let i = 1; i <= 90; i++) {
-    lottoArr.push(i)
-};
-
-const number = (arr) => {
-    return Math.ceil(Math.random() * arr.length);
-};
-
-const draws = (arr) => {
-    for (let i = 1; i <= 5; i++) {
-        const n = number(arr);
-        lottoNums.push(n);
-        // console.log(lottoArr.indexOf(n));
-        lottoArr.splice(arr.indexOf(n), 1);
-        console.log(lottoArr);
-    };
-    return lottoNums;
-};
-
-console.log(draws(lottoArr));
-
-// Majdnem jó, de akár többször is ugyanazt a számot húzza.
