b0d0af1262d85845fc4280ba28ee2681648786e4
Hetesi Roland <rhetesi@gmail.com>
2023-02-08 10:44:09 +0100

    Add: month to the head of the dayspicker table

--- assets/date.js
+++ assets/date.js
@@ -1,49 +1,75 @@
-'use strict';
+"use strict";
 
 export let today = new Date();
 
 // actual-, before- and afterMonth
 export const actualMonth = today;
-export const beforeMonth = new Date(new Date(today.getFullYear(), today.getMonth(), 1) - 1);
-export const afterMonth = new Date(today.getFullYear(), new Date(today.getMonth() + 1), 1);
+export const beforeMonth = new Date(
+  new Date(today.getFullYear(), today.getMonth(), 1) - 1
+);
+export const afterMonth = new Date(
+  today.getFullYear(),
+  new Date(today.getMonth() + 1),
+  1
+);
 
 // dateView options
 export const dateLongView = {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric'
+  year: "numeric",
+  month: "long",
+  day: "numeric",
+};
+export const dateYearAndMonthViev = {
+  year: "numeric",
+  month: "long",
 };
 export const dateMonthView = {
-    month: 'long'
+  month: "long",
 };
 export const dateDateView = {
-    day: 'numeric'
+  day: "numeric",
 };
 
 // Hónap hosszának kiszámítása
 export const monthLength = (month) => {
-    return length = parseInt(new Date(new Date(month.getFullYear(), month.getMonth() + 1, 1) - 1).getDate())
+  return (length = parseInt(
+    new Date(
+      new Date(month.getFullYear(), month.getMonth() + 1, 1) - 1
+    ).getDate()
+  ));
 };
 
 // dateview function, the function get any params (year, month, date) of the date and the viewoption -> then returns the the dateview formatted by the viewoption
 export function dateview(params, viewoption) {
-    let fulldate = {};
+  let fulldate = {};
 
-    params.year ? fulldate.year = params.year : fulldate.year = today.getFullYear();
-    params.month ? fulldate.month = params.month : fulldate.month = today.getMonth();
-    params.date ? fulldate.date = params.date : fulldate.date = today.getDate();
+  params.year
+    ? (fulldate.year = params.year)
+    : (fulldate.year = today.getFullYear());
+  params.month
+    ? (fulldate.month = params.month)
+    : (fulldate.month = today.getMonth());
+  params.date
+    ? (fulldate.date = params.date)
+    : (fulldate.date = today.getDate());
 
-    let dateview = new Date(fulldate.year, fulldate.month, fulldate.date).toLocaleDateString('hu-HU', viewoption);
-    return dateview
+  let dateview = new Date(
+    fulldate.year,
+    fulldate.month,
+    fulldate.date
+  ).toLocaleDateString("hu-HU", viewoption);
+  return dateview;
 }
 
 // Nyomtatvány készítésének dátuma
 export function dateOfPrint(param) {
-    let lastDate;
-    let dateOfPrint;
+  let lastDate;
+  let dateOfPrint;
 
-    param === [param] ? lastDate = new Date(param[param.length - 1]) : lastDate = new Date(param);
+  param === [param]
+    ? (lastDate = new Date(param[param.length - 1]))
+    : (lastDate = new Date(param));
 
-    lastDate >= today ? dateOfPrint = lastDate : dateOfPrint = today;
-    return dateOfPrint.toLocaleDateString('hu-HU', dateLongView);
+  lastDate >= today ? (dateOfPrint = lastDate) : (dateOfPrint = today);
+  return dateOfPrint.toLocaleDateString("hu-HU", dateLongView);
 }
--- assets/main.js
+++ assets/main.js
@@ -1,18 +1,13 @@
-'use strict';
+"use strict";
 
 // import from config
-import {
-    workCity,
-    pricePerKm
-} from "./config.js";
+import { workCity, pricePerKm } from "./config.js";
 
 //Namespace import of date.js module
 import * as dateModule from "./date.js";
 // usage example: const actualMonth = utiktgDateModule.actualMonth();
 
-import {
-    createAnyElement
-} from "./html.js";
+import { createAnyElement } from "./html.js";
 
 // import jsPDF from "jspdf";
 // import autoTable from 'jspdf-autotable';
@@ -21,9 +16,7 @@ import {
  * pdf import as Global module format
  */
 
-const {
-    jsPDF
-} = window.jspdf;
+const { jsPDF } = window.jspdf;
 
 // import jsPDF from "./jspdf.es.min.js";
 // import autoTable from "./jspdf.plugin.autotable.js";
@@ -31,40 +24,58 @@ const {
 
 // basedatas object declaration
 const basedatas = {
-    name: '',
-    city: '',
-    address: '',
-    distance: '',
-    vehicle: '',
-    plate: ''
+  name: "",
+  city: "",
+  address: "",
+  distance: "",
+  vehicle: "",
+  plate: "",
 };
 
 // select some HTML element
-const monthSelect = document.querySelector('.monthselect');
+const monthSelect = document.querySelector(".monthselect");
 // const monthSelectionButton = document.querySelector('.monthselection');
-const daysPicker = document.querySelector('.dayspicker');
-const printButton = document.querySelector('.print');
+const daysPicker = document.querySelector(".dayspicker");
+const printButton = document.querySelector(".print");
 
 let htmlElement;
 
 htmlElement = `
 <div>
 <input type='radio' class='mSelect' id='beforeMonth' name='month' value='beforeMonth'>
-<label for='beforeMonth'>${dateModule.beforeMonth.toLocaleDateString('hu-HU', dateModule.dateMonthView)}</label>
+<label for='beforeMonth'>${dateModule.beforeMonth.toLocaleDateString(
+  "hu-HU",
+  dateModule.dateMonthView
+)}</label>
 <input type='radio' class='mSelect' id='actualMonth' name='month' value='actualMonth'>
-<label for='actualMonth'>${dateModule.actualMonth.toLocaleDateString('hu-HU', dateModule.dateMonthView)}</label>
+<label for='actualMonth'>${dateModule.actualMonth.toLocaleDateString(
+  "hu-HU",
+  dateModule.dateMonthView
+)}</label>
 </div>
 `;
 
 // console.log(monthSelect);
 // monthSelect.insertAdjacentText('beforeend', 'hello');
-monthSelect.insertAdjacentHTML('beforeend', htmlElement);
+monthSelect.insertAdjacentHTML("beforeend", htmlElement);
 
-const fm = document.querySelector('.firstMonth');
-const sm = document.querySelector('.secondMonth');
+const fm = document.querySelector(".firstMonth");
+const sm = document.querySelector(".secondMonth");
 
-fm.insertAdjacentText('beforeend', `${dateModule.beforeMonth.toLocaleDateString('hu-HU', dateModule.dateMonthView)}`);
-sm.insertAdjacentText('beforeend', `${dateModule.actualMonth.toLocaleDateString('hu-HU', dateModule.dateMonthView)}`);
+fm.insertAdjacentText(
+  "beforeend",
+  `${dateModule.beforeMonth.toLocaleDateString(
+    "hu-HU",
+    dateModule.dateMonthView
+  )}`
+);
+sm.insertAdjacentText(
+  "beforeend",
+  `${dateModule.actualMonth.toLocaleDateString(
+    "hu-HU",
+    dateModule.dateMonthView
+  )}`
+);
 
 // daysPicker.insertAdjacentText('beforeend', 'Hello Roland');
 
@@ -77,20 +88,70 @@ sm.insertAdjacentText('beforeend', `${dateModule.actualMonth.toLocaleDateString(
 3.) apeend the generated elemnts to its parent (td -> tr; tr&th -> tbody&thead; tbody&thead -> table; table -> daysPickers)
 */
 const addFullMonth = (month) => {
-    // let viewMonth = month;
+  // let viewMonth = month;
 
-    // tr.appendChild(createAnyElement('th'));
-    // daysPicker
-    //     .appendChild(createAnyElement('table'))
-    //     .appendChild(createAnyElement('thead'))
-    //     .appendChild(createAnyElement('tr'))
-    //     .appendChild(createAnyElement('th'));
+  // tr.appendChild(createAnyElement('th'));
+  // daysPicker
+  //     .appendChild(createAnyElement('table'))
+  //     .appendChild(createAnyElement('thead'))
+  //     .appendChild(createAnyElement('tr'))
+  //     .appendChild(createAnyElement('th'));
 
-    for (let i = 1; i <= dateModule.monthLength(month); i = i + 1) {
-        // daysPicker.insertAdjacentText('beforeend', `${new Date(viewMonth.getFullYear(), viewMonth.getMonth(), i).toLocaleDateString('hu-HU', dateModule.dateLongView)}, `);
-        daysPicker.insertAdjacentText('beforeend', `${new Date(month.getFullYear(), month.getMonth(), i).toLocaleDateString('hu-HU', dateModule.dateLongView)}, `);
+  /*
+    Ez volt a régi hónap lista készítő
+
+    let table = document.querySelector("#gepjarmu");
+    let tBody = table.querySelector("tbody");
+    for (let i = 0; i < actualMonthLength; i++) {
+    let tr = createAnyElement("tr");
+    let td = createAnyElement("td");
+    let day = i + 1;
+    td.innerHTML = actualYear + ". " + monthName[month] + " " + day + ".";
+    tr.appendChild(td);
+    td = createAnyElement("td" , {class: "text-center"} );
+    let checkBox = createAnyElement("input", {
+        type: "checkbox",
+        class: "checkbox",
+        id: day,
+        //name: actualYear + ". " + month + ". " + day + ".", // Ez a hagyományos szüveg összevonás
+        name: `${actualYear}. ${month + 1}. ${day}.`, // Ez pedig sokszor a jobb módszer, mert itt a változók nem string-ként kezeltek.
+        value: date.setFullYear(actualYear, month, day)
+    });
+    td.appendChild(checkBox);
+    tr.appendChild(td);
+    tBody.appendChild(tr);
     }
-}
+    let tr = createAnyElement("tr");
+    let td = createAnyElement("td");
+    tr.appendChild(td);
+    td = createAnyElement("td");
+
+    Idáig
+    */
+  let table = document.querySelector(".daysPickerTable");
+  let tableHead = document.querySelector(".daysPickerTableHead");
+  let tableHeadMonth = tableHead.querySelector(".dayOfMonthColumn");
+  let tableBody = document.querySelector(".dayspicker");
+
+  tableHeadMonth.insertAdjacentText(
+    "beforeend",
+    `${new Date(month.getFullYear(), month.getMonth()).toLocaleDateString(
+      "hu-HU",
+      dateModule.dateYearAndMonthViev
+    )}`
+  );
+
+  for (let i = 1; i <= dateModule.monthLength(month); i = i + 1) {
+    // daysPicker.insertAdjacentText('beforeend', `${new Date(viewMonth.getFullYear(), viewMonth.getMonth(), i).toLocaleDateString('hu-HU', dateModule.dateLongView)}, `);
+    daysPicker.insertAdjacentText(
+      "beforeend",
+      `${new Date(month.getFullYear(), month.getMonth(), i).toLocaleDateString(
+        "hu-HU",
+        dateModule.dateLongView
+      )}, `
+    );
+  }
+};
 
 addFullMonth(dateModule.actualMonth);
 // const doc = new jsPDF();
