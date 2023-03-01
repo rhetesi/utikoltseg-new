68bf0eb9573383e3bb43dd0d772c5b6be2d87b4c
Hetesi Roland <rhetesi@gmail.com>
2023-03-01 14:50:07 +0100

    Add datemodule import ot createpdf.js & create the print function w/o any function

--- assets/createpdf.js
+++ assets/createpdf.js
@@ -1,6 +1,12 @@
 "use strict";
 
+import * as date from "./date.js";
+
 const { jsPDF } = window.jspdf;
+
+const print = ({ params }) => {
+  const pdf = new jsPDF();
+};
 
 const pdfDoc = new jsPDF();
 pdfDoc.text(`Munkába járás elszámolása`, 105, 10, "center");
